258dfb7f0227b3a1d21596317fee6b51c2dda5fe
F1reStyLe <iwgw@bk.ru>
2023-02-21 15:15:10 +0300

    add profile VT url product base.html

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login, register
+from .views import login, register, profile
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration', register, name='registration'),
+    path('profile/', profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -33,3 +33,7 @@ def register(request):
     context = {'form': form}
     return render(request, 'users/register.html', context)
 
+
+def profile(request):
+    return render(request, 'users/profile.html')
+
