c44f7df8c0ba9bec0d7d1d55f5e145d71a6c07d9
F1reStyLe <iwgw@bk.ru>
2023-02-24 16:53:08 +0300

    add basket_add view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import index, products
+from products.views import index, products, basket_add
 
 urlpatterns = [
     path('', index, name="index"),
     path('products/', products, name="products"),
+    path('products/baskets/add/<int:product_id>', basket_add, name='basket_add')
 ]
