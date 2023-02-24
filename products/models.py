4c455e57c5122bd7f5dd0ff3914598c893433601
F1reStyLe <iwgw@bk.ru>
2023-02-24 16:27:27 +0300

    add basket model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import User
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, null=False, db_index=True, verbose_name="Имя категории")
@@ -27,3 +27,17 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Время добавлеения')
+
+    def __str__(self):
+        return f'Корзина {self.user.username} | Продукт {self.product.name}'
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
