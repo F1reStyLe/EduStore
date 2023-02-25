94b3499cc2dea29a54093d0bcfc89a9c55ceec02
F1reStyLe <iwgw@bk.ru>
2023-02-25 09:50:56 +0300

    add category filter to sidebar

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from products.views import index, products, basket_add, basket_remove
 urlpatterns = [
     path('', index, name="index"),
     path('products/', products, name="products"),
+    path('category/<int:category_id>', products, name="category"),
     path('products/baskets/add/<int:product_id>', basket_add, name='basket_add'),
     path('products/baskets/remove/<int:basket_id>', basket_remove, name='basket_remove'),
 ]
