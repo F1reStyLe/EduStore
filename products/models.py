46e8f3622feedee47eeaedd0f1c64b27489f346f
F1reStyLe <iwgw@bk.ru>
2023-02-25 09:14:06 +0300

    add BasketQSet and methods sum, total_sum, total_quantity

--- products/models.py
+++ products/models.py
@@ -29,14 +29,27 @@ class Product(models.Model):
         verbose_name_plural = "Продукты"
 
 
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Пользователь')
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Продукт')
     quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Время добавлеения')
 
+    object = BasketQuerySet.as_manager()
+
     def __str__(self):
         return f'Корзина {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
 
     class Meta:
         verbose_name = 'Корзина'
