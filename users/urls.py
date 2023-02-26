c6651f2ecad33951c23fee121d456a9fe479d268
F1reStyLe <iwgw@bk.ru>
2023-02-26 14:23:09 +0300

    registation -> UserRegistrationView

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login, register, profile, logout
+from .views import login, UserRegistrationView, profile, logout
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration', register, name='registration'),
+    path('registration', UserRegistrationView.as_view(), name='registration'),
     path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
+from django.views.generic import CreateView
+
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from products.models import Basket
+from .models import User
 
 
 def login(request):
@@ -24,17 +27,25 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
-def register(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Пользователь успешно зарегистрирован')
-            return HttpResponseRedirect(reverse('login'))
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/register.html', context)
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+
+
+# def register(request):
+#     if request.method == "POST":
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Пользователь успешно зарегистрирован')
+#             return HttpResponseRedirect(reverse('login'))
+#     else:
+#         form = UserRegistrationForm()
+#     context = {'form': form}
+#     return render(request, 'users/register.html', context)
 
 
 @login_required
